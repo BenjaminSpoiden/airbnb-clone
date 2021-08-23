893568f1737025de1b44fc65e08092d181abede8
BenjaminSpoiden <spoiden.benjamin@hotmail.com>
2021-08-23 17:50:19 +0200

    setting up backend for airbnb clone

---
+++ schemas/person.js
@@ -0,0 +1,41 @@
+export default {
+    name: 'person',
+    title: 'Person',
+    type: 'document',
+    fields: [
+        {
+            name: 'firstName',
+            title: 'First Name',
+            type: 'string'
+        }, 
+        {
+            name: 'lastName',
+            title: 'Last Name',
+            type: 'string'
+        }, 
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug', 
+            options: {
+                source: 'name',
+                maxLength: 100
+            }
+        },
+        {
+            name: 'id',
+            title: 'ID',
+            type: 'number'
+        }, 
+        {
+            name: 'photo',
+            title: 'Photo',
+            type: 'image'
+        }
+    ],
+    preview: { 
+        select: {
+            title: 'firstName', media: 'photo'
+        }
+    }
+}
---
+++ schemas/review.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'review',
+    title: 'Review',
+    type: 'object',
+    fields: [
+        {
+            name: 'reviewDesc',
+            title: 'Review Description',
+            type: 'string'
+        }, 
+        // {
+        //     name: 'traveller',
+        //     title: 'Traveller',
+        //     type: 'traveller'
+        // }, 
+        {
+            name: 'rating',
+            title: 'Rating',
+            type: 'string',
+            options: {
+                list: [ 
+                    { title: '5 Stars', value: 'five-stars' }, 
+                    { title: '4 Stars', value: 'four-stars' }, 
+                    { title: '3 Stars', value: 'three-stars' }, 
+                    { title: '2 Stars', value: 'two-stars' }, 
+                    { title: '1 Stars', value: 'one-stars' }, 
+                ],
+                layout: 'radio'
+            }
+        }
+    ]
+}
